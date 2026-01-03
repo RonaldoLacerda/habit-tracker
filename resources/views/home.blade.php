389661747ee754d7a48a6d0df45dfa72e317f2c7
Ronaldo Lacerda <ronaldolacerdadasilva@gmail.com>
2026-01-03 19:21:41 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
-<h1>
-    Welcome to the home page
-</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>
+    {{ config('app.name') }}
+  </title>
 
-<p>
-    Hello, {{ $nome }}! Here are your habits:
-    <ul>
-        @foreach ($habits as $habit)
-            <li>{{ $habit }}</li>
-        @endforeach
-    </ul>
-</p>
+  @vite('resources/css/app.css')
 
-@auth
-    <p>
-        Logado!
-    </p>
-@endauth
-@guest
-    <p>
-        Não logado!
-    </p>
-@endguest
+</head>
+<body>
+
+  <h1 class="text-3xl font-bold underline">
+    Hello, {{ $nome }}!
+
+  </h1>
+
+</body>
+</html>
