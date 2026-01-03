09419ccdc7cdd15830162b1a5610a43b487822af
Ronaldo Lacerda <ronaldolacerdadasilva@gmail.com>
2026-01-03 20:26:35 -0300

    feat(Auth): implement login form and authentication logic

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,5 +3,41 @@
     <h1>
       Faça Login
     </h1>
+
+    <section>
+      <form action="/login" method="POST">
+        @csrf
+
+        @error('email')
+          <p class="text-red-500 text-md mt-2">
+            {{ $message }}
+          </p>
+        @enderror
+
+
+        <input
+          type="email"
+          name="email"
+          placeholder="your@email.com"
+          class="bg-white p-2 border-2"
+        >
+
+        <input
+          type="password"
+          name="password"
+          placeholder="********"
+          class="bg-white p-2 border-2"
+        >
+
+        <button
+          type="submit"
+          class="bg-white p-2 border-2"
+        >
+          Entrar
+        </button>
+
+      </form>
+    </section>
+
   </main>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index']);
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']); //login
+Route::post('/login', [LoginController::class, 'authenticate']); //login
