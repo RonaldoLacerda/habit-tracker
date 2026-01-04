2ea8565d710f38927717fa05a75bd1804d007149
Ronaldo Lacerda <ronaldolacerdadasilva@gmail.com>
2026-01-03 21:03:01 -0300

    feat(LoginRequest): create form request and improve show error

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,34 +1,54 @@
 <x-layout>
   <main class="py-10">
-    <h1>
-      Faça Login
-    </h1>
+    <section class="bg-white max-w-[600px] mx-auto p-10 border-2 mt-4">
 
-    <section>
-      <form action="/login" method="POST">
+      <h1 class="font-bold text-xl">
+        Faça Login
+      </h1>
+
+      <p>
+        Insira seus dados para acessar.
+      </p>
+
+      <form action="/login" method="POST" class="flex flex-col">
         @csrf
 
-        @error('email')
-          <p class="text-red-500 text-md mt-2">
-            {{ $message }}
-          </p>
-        @enderror
+        <div class="flex flex-col gap-2 mb-4">
+          <label for="email">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            class="bg-white p-2 border-2 @error ('email') border-red-500 @enderror"
+          >
+          @error('email')
+            <p class="text-red-500">
+              {{ $message }}
+            </p>
+          @enderror
 
+        </div>
 
-        <input
-          type="email"
-          name="email"
-          placeholder="your@email.com"
-          class="bg-white p-2 border-2"
-        >
+        <div class="flex flex-col gap-2 mb-4">
+          <label for="password">
+            Senha
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="********"
+            class="bg-white p-2 border-2 @error ('password') border-red-500 @enderror"
+          >
 
-        <input
-          type="password"
-          name="password"
-          placeholder="********"
-          class="bg-white p-2 border-2"
-        >
+          @error('password')
+            <p class="text-red-500">
+              {{ $message }}
+            </p>
+          @enderror
 
+        </div>
         <button
           type="submit"
           class="bg-white p-2 border-2"
